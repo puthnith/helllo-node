bfbe27fcb4097f42ff858abf4dd3cdecdf3f8cb4
Puthnith <puthnith@gmail.com>
2020-07-17 16:25:00 +0200

    test: add unit tests for camel and snake fns

--- src/camel-snake.test.ts
+++ src/camel-snake.test.ts
@@ -1,5 +1,25 @@
-describe("example", () => {
-  test("true", () => {
-    expect(true).toBeTruthy()
+import { camel, snake } from "./camel-snake"
+
+describe("camel function", () => {
+  it.each`
+    test             | expected
+    ${"Hello World"} | ${"HelloWorld"}
+    ${"first_name"}  | ${"firstName"}
+    ${"lastName"}    | ${"lastName"}
+  `(`Should turn "$test" into "$expected"`, (args) => {
+    const { test, expected } = args as { test: string; expected: string }
+    expect(camel(test)).toBe(expected)
+  })
+})
+
+describe("snake function", () => {
+  it.each`
+    test             | expected
+    ${"Hello World"} | ${"hello_world"}
+    ${"lastName"}    | ${"last_name"}
+    ${"first_name"}  | ${"first_name"}
+  `(`Should turn "$test" into "$expected"`, (args) => {
+    const { test, expected } = args as { test: string; expected: string }
+    expect(snake(test)).toBe(expected)
   })
 })
